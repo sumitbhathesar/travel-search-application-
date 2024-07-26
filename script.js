60e42062b7b01577b706c9241feeaebf0130596f
sumit bhathesar <121190294+sumitbhathesar@users.noreply.github.com>
2024-07-26 14:36:18 +0530

    Add files via upload
    Travel Search Application
    
    A web application built using HTML, CSS, and JavaScript that allows users to explore different destinations in India. This project was completed as part of my internship with CodeClause.
    
    Features:
    
    Responsive design using HTML and CSS
    Interactive elements using JavaScript
    Brief descriptions of each destination
    "Plan Your Trip" button for users to plan their trip
    Technologies used:
    
    HTML
    CSS
    JavaScript

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('search-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Search submitted!');
+});
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Message sent!');
+});
